0ca141d9bbfda8525e25a0cf5e4d1cfd353a2a2b
Sajad Haibat <40578653+sajadhaibat@users.noreply.github.com>
2020-02-16 22:22:39 +0430

    Auth service and auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -45,12 +46,13 @@ import { LoginComponent } from './login/login.component';
           { path: '', component: HomeComponent },
           { path: 'products', component: ProductsComponent },
           { path: 'shopping-cart', component: ShoppingCartComponent },
-          { path: 'check-out', component: CheckOutComponent },
-          { path: 'order-success', component: OrderSuccessComponent },
-          { path: 'my/orders', component: MyOrdersComponent },
           { path: 'login', component: LoginComponent },
-          { path: 'admin/products', component: AdminProductsComponent },
-          { path: 'admin/orders', component: AdminOrdersComponent }
+
+          { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
+          { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
+          { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
+          { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard] },
+          { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard] }
       ])
   ],
   providers: [],
